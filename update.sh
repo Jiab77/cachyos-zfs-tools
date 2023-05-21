2790245271665a633c7ed428fb83c376866cbbea
Jiab77 <jiab77@pm.me>
2023-05-21 19:16:52 +0200

    ✨ Added missing `update` script

---
+++ update.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Basic updater script for CachyOS ZFS tools
+# Made by Jiab77 - 2023
+#
+# Thanks to vnepogodin for the inspiration of the 'die' method :)
+#
+# Version 0.0.0
+
+# Options
+set -o xtrace
+
+# Config
+PROJECT_NAME="CachyOS ZFS tools/scripts"
+
+# Functions
+function get_version() {
+    grep -i 'version' "$0" | awk '{ print $3 }' | head -n1
+}
+function die() {
+    echo -e "\nError: $*\n"
+    exit 255
+}
+
+# Header
+echo -e "\nSimple $PROJECT_NAME updater - v$(get_version)\n"
+
+# Usage
+[[ $1 == "-h" && $1 == "--help" ]] && echo -e "\nUsage: $(basename "$0") -- Update $PROJECT_NAME.\n" && exit 0
+
+# Checks
+[[ $(id -u) -ne 0 ]] && die "You must run this script as root or with 'sudo'."
+
+# Main
+./uninstall.sh
+./install.sh
